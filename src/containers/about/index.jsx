24c656791edc6c015a6cfe8e52dec41d5a2b4fdf
Drashti Gupta <drashtigupta017@gmail.com>
2025-06-24 15:35:44 +0530

    portfolio reactjs

---
+++ src/containers/about/index.jsx
@@ -0,0 +1,115 @@
+import React from "react";
+import { BsInfoCircleFill } from "react-icons/bs";
+import PageHeaderContent from "../../components/pageHeaderContent";
+import { Animate } from "react-simple-animate";
+import "./styles.scss";
+import { DiApple, DiAndroid } from "react-icons/di";
+import { FaDev, FaDatabase } from "react-icons/fa";
+
+const personalDetails = [
+  {
+    label: "Name",
+    value: "Drashti Gupta",
+  },
+  {
+    label: "Age",
+    value: "21",
+  },
+  {
+    label: "Address",
+    value: "India",
+  },
+  {
+    label: "Email",
+    value: "drashtigupta017@gmail.com",
+  },
+  {
+    label: "Contact No",
+    value: "+91 8126146610",
+  },
+];
+
+const jobSummary =
+  "My name is Drashti Gupta, and I’m a passionate and detail-oriented software developer with a strong foundation in JavaScript, React.js, and backend technologies like Express.js and MongoDB. As a Katalyst trainee, I’ve had the opportunity to enhance both my technical and professional skills through structured training and mentorship.";
+
+const About = () => {
+  return (
+    <section id="about" className="about">
+      <PageHeaderContent
+        headerText="About Me"
+        icon={<BsInfoCircleFill size={40} />}
+      />
+      <div className="about__content">
+        <div className="about__content__personalWrapper">
+          <Animate
+            play
+            duration={1.5}
+            delay={1}
+            start={{
+              transform: "translateX(-900px)",
+            }}
+            end={{
+              transform: "translatex(0px)",
+            }}
+          >
+            <h3>Summary</h3>
+            <p>{jobSummary}</p>
+          </Animate>
+
+          <Animate
+            play
+            duration={1.5}
+            delay={1}
+            start={{
+              transform: "translateX(500px)",
+            }}
+            end={{
+              transform: "translatex(0px)",
+            }}
+          >
+            <h3 className="personalInformationHeaderText">
+              Personal Information
+            </h3>
+            <ul>
+              {personalDetails.map((item, i) => (
+                <li key={i}>
+                  <span className="title">{item.label}</span>
+                  <span className="value">{item.value}</span>
+                </li>
+              ))}
+            </ul>
+          </Animate>
+        </div>
+        <div className="about__content__servicesWrapper">
+          <Animate
+            play
+            duration={1.5}
+            delay={1}
+            start={{
+              transform: "translateX(600px)",
+            }}
+            end={{
+              transform: "translatex(0px)",
+            }}
+          >
+            <div className="about__content__servicesWrapper__innerContent">
+              <div>
+                <FaDev size={60} color="var( --yellow-theme-main-color)" />
+              </div>
+              <div>
+                <DiAndroid size={60} color="var( --yellow-theme-main-color)" />
+              </div>
+              <div>
+                <FaDatabase size={60} color="var( --yellow-theme-main-color)" />
+              </div>
+              <div>
+                <DiApple size={60} color="var( --yellow-theme-main-color)" />
+              </div>
+            </div>
+          </Animate>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default About;
---
+++ src/containers/resume/utils.js
@@ -0,0 +1,31 @@
+export const data = {
+    experience: [
+      {
+        title: "Frontend developer",
+        subTitle: "Internship",
+        description:
+          " project and training conducted under IBM mentorship and guidelines",
+      },
+      
+    ],
+    education: [
+      {
+        title: "Secondary",
+        subTitle: "D.S.B.M Sr.Sec.School",
+        description:
+          "I got 89% in my 10th Grade",
+      },
+      {
+        title: "Higher Secondary",
+        subTitle: "D.S.B.M Sr. Sec.School",
+        description:
+          "i got 84% in my higher secondary ",
+      },
+      {
+        title: "B.Tech",
+        subTitle: "Krishna Engineering College ",
+        description:
+          "Bachelor of technology in Information Technology grade-8.0",
+      },
+    ],
+  };
